6a39892b26562c370a2ea41939ccdd3a4c35c5ad
aamoghS <aamoghsawantt@gmail.com>
2026-03-04 19:34:13 -0500

    hello

--- sites/hacklytics2026/eslint.config.mjs
+++ sites/hacklytics2026/eslint.config.mjs
@@ -1,25 +1,33 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import tseslint from "typescript-eslint";
+import nextPlugin from "@next/eslint-plugin-next";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  // keep Next.js recommended configs
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-
-  // custom overrides
+/** @type {import("eslint").Linter.Config[]} */
+export default tseslint.config(
   {
-    rules: {
-      "react/no-unescaped-entities": "off",
-      "@next/next/no-img-element": "off",
-    },
+    ignores: [".next/**", "node_modules/**", "out/**"],
   },
-];
-
-export default eslintConfig;
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        projectService: true,
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint.plugin,
+      "@next/next": nextPlugin,
+    },
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs["core-web-vitals"].rules,
+      "@next/next/no-duplicate-head": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-img-element": "off"
+    },
+  }
+);
