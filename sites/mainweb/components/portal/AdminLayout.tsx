be15cc273fcdd6dd8888ffe0cec6320a60cd2473
aamoghS <aamoghsawantt@gmail.com>
2026-04-29 14:33:09 -0400

    ok

--- sites/mainweb/components/portal/AdminLayout.tsx
+++ sites/mainweb/components/portal/AdminLayout.tsx
@@ -1,9 +1,9 @@
 'use client';
 
 import { useSession } from 'next-auth/react';
+import AdminSidebar from './AdminSidebar';
+import AdminHeader from './AdminHeader';
 import { useEffect, useState } from 'react';
-import Background from './Background';
-import { Spinner } from './Spinner';
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
   const { data: session, status } = useSession();
@@ -21,16 +21,17 @@ export default function AdminLayout({ children }: { children: React.ReactNode })
     return (
       <div className="relative min-h-screen bg-[#050505]">
         <div className="flex items-center justify-center h-screen">
-          <Spinner className="w-8 h-8 text-[#00A8A8]" />
+          <div className="h-8 w-8 animate-spin rounded-full border-2 border-[#00A8A8] border-t-transparent" />
         </div>
       </div>
     );
   }
 
   return (
-    <div className="relative min-h-screen bg-[#050505] text-gray-400 font-sans selection:bg-[#00A8A8]/30 overflow-x-hidden">
-      <Background className="fixed inset-0 z-0 opacity-[0.03]" />
-      <div className="relative z-10 flex flex-col min-h-screen">
+    <div className="relative min-h-screen bg-[#0a0c10] dark:bg-darkBlue/80 text-gray-400 font-sans selection:bg-[#00A8A8]/30 overflow-x-hidden">
+      <AdminSidebar />
+      <AdminHeader />
+      <div className="w-full ml-64 pt-16">
         {children}
       </div>
     </div>
